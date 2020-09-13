79a2f5ee428232abc05f4e2733ca024f5bd5b892
Jesus <jjhurtado016@gmail.com>
2020-09-13 14:57:56 -0300

    modify config

--- global/config.js
+++ global/config.js
@@ -8,8 +8,10 @@ let urlDB;
 
 if (process.env.NODE_ENV = 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
+    console.log('estas en dev')
 } else {
     urlDB = 'mongodb+srv://Chuyto70:daqa1503@cluster0.cgvyq.mongodb.net/cafe'
+    console.log('Estas en produccion');
 }
 
 process.env.URLDB = urlDB
